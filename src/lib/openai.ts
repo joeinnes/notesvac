839ddcec07504d8c481319d56b7d7fd72d210498
Joe Innes <joe@joeinn.es>
2022-01-09 00:08:17 +0100

    fix(platform): :bug: Remove process.env to allow non-node hosting

--- src/lib/openai.ts
+++ src/lib/openai.ts
@@ -1,10 +1,8 @@
 import OpenAI from 'openai-api';
-import dotenv from 'dotenv';
-dotenv.config();
 
 // Load your key from an environment variable or secret management service
 // (do not include your key directly in your code)
-const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
+const OPENAI_API_KEY = import.meta.env.OPENAI_API_KEY + '';
 const openai = new OpenAI(OPENAI_API_KEY);
 
 export default openai;
