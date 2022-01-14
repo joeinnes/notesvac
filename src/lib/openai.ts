3dc66b63865a2825bda5756a22a1dbd79bbd2203
Joe Innes <joe@joeinn.es>
2022-01-14 18:22:25 +0100

    Make conditional import clearer

--- src/lib/openai.ts
+++ src/lib/openai.ts
@@ -1,5 +1,7 @@
 import OpenAI from 'openai-nodejs';
-import(!process.env.CF_PAGES ? 'dotenv/config' : '');
+if (!process.env.CF_PAGES) {
+  import('dotenv/config');
+}
 // Load your key from an environment variable or secret management service
 // (do not include your key directly in your code)
 const OPENAI_API_KEY = process.env.OPENAI_API_KEY + '';
