28cdac1592cfa86fc49589c8a038804d0b2336ed
Joe Innes <joe@joeinn.es>
2022-02-02 14:03:06 +0100

    Back to auto. No idea why it won't build

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 import preprocess from 'svelte-preprocess';
-import adapter from '@sveltejs/adapter-vercel';
+import adapter from '@sveltejs/adapter-auto';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
