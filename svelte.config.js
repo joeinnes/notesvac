a1e3c639678618eb50dbc186c1d11b803d22f001
Joe Innes <joe@joeinn.es>
2022-02-02 15:04:22 +0100

    Move adapter into kit

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,13 @@
 /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 import preprocess from 'svelte-preprocess';
-import adapter from '@sveltejs/adapter-vercel';
+import adapter from '@sveltejs/adapter-auto';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: [preprocess({ postcss: true })],
-	adapter: adapter(),
 	kit: {
-		target: '#svelte'
+		target: '#svelte',
+		adapter: adapter()
 	}
 };
 
